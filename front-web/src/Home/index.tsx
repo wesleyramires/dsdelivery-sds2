8614b20c2be8bea66d6e4153ec2e1b6d58071ab6
Wesley Ramires <ramirescristao@gmail.com>
2021-01-06 13:05:42 -0300

    Add Home Page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,32 @@
+import './styles.css';
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br /> que entregamos <br /> pra você!!!
+                    </h1>
+
+                    <h3 className="home-subtitle">
+                        Escolha seu pedido e em poucos minutos <br /> levaremos na sua porta.
+                    </h3>
+
+                    <h3>
+                        <a href="link" className="home-btn-order"> 
+                            FAZER PEDIDO
+                        </a>
+                    </h3>
+                </div>
+
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
